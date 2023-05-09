43016b2a17d6b2316a040f574c010ccbbb12d273
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-09 13:17:49 +0100

    Functionalizing the Escape Key

--- script.js
+++ script.js
@@ -8,19 +8,25 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnsOpenModal);
 
 const openModal = function () {
-    // console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  }
+  // console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  };
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++)
   btnOpenModal[i].addEventListener('click', openModal);
 
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
