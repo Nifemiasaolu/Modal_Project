4e0d0c6e8b582168ba7e90ee66252f2f14135233
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-15 14:50:54 +0100

    Final script commit

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 'use strict';
 
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
-// const btnOpenModal = document.querySelectorAll('.show-modal');
-// // console.log(btnsOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-// const openModal = function () {
-//   // console.log('Button clicked');
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 
-// for (let i = 0; i < btnOpenModal.length; i++)
-//   btnOpenModal[i].addEventListener('click', openModal);
+  document.addEventListener('keydown', function (e) {
+    // console.log(e.key);
 
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   // console.log(e.key);
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
-
-
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+}
