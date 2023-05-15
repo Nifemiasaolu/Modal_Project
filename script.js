dbd6f3f9b9fe4c203aa12cd7ca7ce04f10622f24
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-15 14:20:35 +0100

    New script

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
 
-const btnOpenModal = document.querySelectorAll('.show-modal');
-// console.log(btnsOpenModal);
+// const btnOpenModal = document.querySelectorAll('.show-modal');
+// // console.log(btnsOpenModal);
 
-const openModal = function () {
-  // console.log('Button clicked');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+// const openModal = function () {
+//   // console.log('Button clicked');
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnOpenModal.length; i++)
+//   btnOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   // console.log(e.key);
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
+
+
